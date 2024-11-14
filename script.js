c623546f1e65a20c5165c3bd1b0217c637e225ae
Nogi3201 <hasanpohan035@gmail.com>
2024-11-14 23:53:54 +0700

    try

--- script.js
+++ script.js
@@ -1,34 +1,3 @@
-let slideshow = 1;
-showSlides(slideshow);
-
-function plusSlides(n) {
-    showSlides(slideshow += n);
-}
-
-function currentSlide(n) {
-    showSlides(slideshow = n);
-}
-
-function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("slides");
-    let dots = document.getElementsByClassName("dot");
-
-    if (n > slides.length) { slideshow = 1 }    
-    if (n < 1) { slideshow = slides.length }
-
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
-    }
-
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-
-    slides[slideshow - 1].style.display = "block";  
-    dots[slideshow - 1].className += " active";
-}
-
 let slideIndex = 1;
 showSlides(slideIndex);
 
